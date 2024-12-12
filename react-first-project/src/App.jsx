9a1e310e2cab2e25551dda2c7966cd4af4da6d47
aarshpatel12345 <aarsh18patel@gmail.com>
2024-12-12 12:47:21 +0530

    initial commit

---
+++ react-first-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Aarsh Patel</h1>
+      <h2>A Full Stack Web Developer</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
